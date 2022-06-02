ec9dfb5e9bbe85ca76e8863aed33092a10b47d9c
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-02 21:44:43 +0530

    Added: Part time added and calculated daily wage for part time employee also

---
+++ src/UC3_PartTimeAdded.java
@@ -0,0 +1,25 @@
+public class UC3_PartTimeAdded {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+
+        //Initializing variables
+        int isFullTime = 1;
+        int isPartTime = 2;
+        int empRatePerHour = 20;
+        int empDailyWage = 0;
+        int empHours = 0;
+
+        /*Checked if employee is full time working or part time or absent*/
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == isFullTime)
+            empHours = 8;
+        else if (empCheck == isPartTime)
+            empHours = 4;
+        else
+            empHours = 0;
+
+        //Calculating employee's daily wage
+        empDailyWage = empHours * empRatePerHour;
+        System.out.println("EMPLOYEE DAILY WAGE IS:" + empDailyWage);
+    }
+}
